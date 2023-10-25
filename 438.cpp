5a4cee975e6c16f569e25f47c3fe8333fc6685d6
rkzthdu <68888733+rkzthdu@users.noreply.github.com>
2023-10-25 21:04:05 +0800

    Update 438.cpp

--- 438.cpp
+++ 438.cpp
@@ -1,6 +1,59 @@
+/*
+题目描述：
+给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。
+异位词 指由相同字母重排列形成的字符串（包括相同的字符串）。
+
+我的思路：
+1、统计字符串p所有字符出现的个数，以字符为key、格数为value建立hashmap，用于判断s的字串是否为p的“异位词”;
+2、用滑动窗口逐步检测s长为p.size()的子串;
+3、若上一个子串是p的“异位词”，则滑动时只要判断移除窗口的字符与移进窗口的字符是否相等
+4、剪枝操作，窗口可以跳过部分子串
+*/
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-
+        vector<int> ans = {};
+        int np = p.size(), sp = s.size();
+        if(np > sp) {
+            return ans;
+        }
+        unordered_map<char, int> nletter;
+        for(auto c: p) {
+            if(nletter.find(c) == nletter.end()) {
+                nletter.emplace(c, 1);
+            } else {
+                nletter[c] += 1;
+            }
+        }
+        bool last = false;
+        for(int i = 0; i < sp - np + 1; ++i) {
+            if(last) {
+                if(s[i - 1] == s[i + np - 1]) {
+                    ans.emplace_back(i);
+                    continue;
+                } else {
+                    last = false;
+                    continue;
+                }
+            }
+            unordered_map<char, int> tmp(nletter);
+            for(int j = 0; j < np; ++j) {
+                if(tmp.find(s[i + j]) == tmp.end()) {
+                    i = i + j;
+                    break;
+                } else {
+                    if(tmp[s[i + j]] == 0) {
+                        break;
+                    } else {
+                        tmp[s[i + j]] -= 1;
+                    }
+                }
+                if(j == np - 1) {
+                    ans.emplace_back(i);
+                    last = true;
+                }
+            }
+        }
+        return ans;
     }
 };
